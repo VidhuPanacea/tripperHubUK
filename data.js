7b5a100e4347d2f76205efd42d33dde3bedaa0a0
Vidhu Shekhar <twrvidhu18@gmail.com>
2023-09-26 12:45:24 +0530

    update

---
+++ data.js
@@ -0,0 +1,157 @@
+const packages = [{
+    id: 1,
+    name: "Delhi Nainital Tour",
+    duration: "3 Nights / 4 Days",
+    destinations: "Nainital, Uttarakhand",
+    category: "Family",
+    price: "19,999",
+    image: "https://images.unsplash.com/photo-1590265787538-4dc8fdeb9651?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80;",
+    description: "Very good package",
+    itinerary: [{ day1: "Start", day2: "end" }],
+    suggested: [{}]
+}, {
+    id: 2,
+    name: "Nainital Ranikhet Tour",
+    duration: "4 Nights / 5 Days",
+    destinations: "Nainital, Ranikhet",
+    category: "Honeymoon",
+    price: "24,999",
+    image: "https://images.unsplash.com/photo-1608942025318-1191eeade556?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2055&q=80",
+    description: "",
+    suggested: [{}]
+},
+{
+    id: 3,
+    name: "Mussorie Rishikesh Tour",
+    duration: "3 Nights / 4 Days",
+    destinations: "Mussorie, Rishikesh",
+    category: "Adventure",
+    price: "49,999",
+    image: "https://images.unsplash.com/photo-1603867106100-0d2039fc8757?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2081&q=80",
+    description: "",
+    suggested: [{}]
+}, {
+    id: 4,
+    name: "Mussorie Honeymoon Tour",
+    duration: "4 Nights / 5 Days",
+    destinations: "Mussorie",
+    category: "Honeymoon",
+    price: "25,999",
+    image: "https://images.unsplash.com/photo-1490031781863-29b9bdbea131?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2071&q=80",
+    description: "",
+    suggested: [{}]
+}, {
+    id: 5,
+    name: "Nainital Corbett Tour",
+    duration: "4 Nights / 5 Days",
+    destinations: "Nainital, Corbett",
+    category: "Wildlife",
+    price: "39,999",
+    image: "https://images.unsplash.com/photo-1669021820355-7186908380d9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
+    description: "",
+    suggested: [{}]
+}, {
+    id: 6,
+    name: "Haridwar and Rishikesh Tour",
+    duration: "2 Nights / 3 Days",
+    destinations: "Haridwar, Rishikesh",
+    category: "Adventure",
+    price: "19,999",
+    image: "https://images.unsplash.com/photo-1511754863001-18d44abd0a93?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
+    description: "",
+    suggested: [{}]
+}, {
+    id: 7,
+    name: "Delhi Landsdowne Weekend Tour",
+    duration: "2 Nights / 3 Days",
+    destinations: "Delhi, Lansdowne",
+    category: "Trekking & Hiking",
+    price: "19,999",
+    image: "https://images.unsplash.com/photo-1511754863001-18d44abd0a93?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
+    description: "",
+    suggested: [{}]
+},
+{
+    id: 8,
+    name: "Auli Skiing Tour",
+    duration: "10 Nights / 11 Days",
+    destinations: "Auli",
+    category: "Adventure",
+    price: "45,999",
+    image: "https://images.unsplash.com/photo-1605540436563-5bca919ae766?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1469&q=80",
+    description: "",
+    suggested: [{}]
+},
+{
+    id: 9,
+    name: "River Rafting Tour",
+    duration: "1 Nights / 2 Days",
+    destinations: "Bhrampuri to Rishikesh",
+    category: "Adventure",
+    price: "35,999",
+    image: "https://images.unsplash.com/photo-1641584495061-89b9025f563b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
+    description: "",
+    suggested: [{}]
+}]
+
+
+// Selecting the DOM Elements
+var app = document.querySelector('#app');
+
+// Mapping the Cards on the main page
+packages.map(function (pkg) {
+    //    creates a DOM element (card) and appends in the div with id - #app 
+    const div = document.createElement("div")
+    div.className = "col-md-4 fadeInUp ftco-animate ftco-animated"
+    div.innerHTML = `<div class="project-wrap">
+        <a name="destinationcard" href = 'packageDetails.html?id=${pkg.id}&name=${pkg.name}'  class="img"
+            style="background-image: url(${pkg.image})">
+            <span class="price">From ₹${pkg.price}</span>
+        </a>
+        <div class="text p-4">
+            <span class="days">${pkg.duration}</span>
+            <h3><a href="#">${pkg.name}</a></h3>
+            <p class="location"><span class="fa fa-map-marker"></span> ${pkg.destinations}</p>
+            <ul>
+                <li><span class="flaticon-shower"></span>2</li>
+                <li><span class="flaticon-king-size"></span>3</li>
+                <li><span class="flaticon-mountains"></span>Near Mountain</li>
+            </ul>
+        </div>
+    </div>`
+    app.appendChild(div) //finally appends the above code into the DOM
+
+
+});
+
+
+//selects the Element on the detialed package page
+var detailed = document.querySelector('#detailed');
+
+// function to filter and render the package on the respective page accordingly
+function detailedPackage(id) {
+    let pkg = packages.filter(p => p.id == id)[0];
+    const div = document.createElement("div")
+    div.innerHTML = `<div class="col-md-4">
+        <div class="project-wrap">
+            <a name="destinationcard"   class="img"
+                style="background-image: url(https://images.unsplash.com/photo-1590265787538-4dc8fdeb9651?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80;)">
+                <span class="price">₹550/person</span>
+            </a>
+            <div class="text p-4">
+                <span class="days">${pkg.duration}</span>
+                <h3><a href="#">${pkg.name}</a></h3>
+                <p class="location"><span class="fa fa-map-marker"></span> Nainital, Uttarakhand</p>
+                <ul>
+                    <li><span class="flaticon-shower"></span>2</li>
+                    <li><span class="flaticon-king-size"></span>3</li>
+                    <li><span class="flaticon-mountains"></span>Near Mountain</li>
+                </ul>
+            </div>
+        </div>
+    </div>`
+    detailed.appendChild(div)
+    return pkg;
+
+}
+
