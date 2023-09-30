812ce359d795e23e7c5cb8d1c083e9550a084018
amit-tiwari-gcp <amit.tiwari.gcp@gmail.com>
2023-09-30 21:24:04 +0530

    sad

---
+++ itinerary.1.js
@@ -0,0 +1,129 @@
+const itineraries = [{
+    id: 1,
+    packageId: 1,
+    itinerary: `<div class="itinerary-cont accordion-box">
+    <h3>Itinerary</h3>
+    <div class="itinerary-cont-box accordion-item is-active">
+        <span>Day 1</span>
+        <div class="accordion-thumb">
+            <h4>DELHI - NAINITAL (320 KMS / 08 HRS. DRIVE)</h4>
+            <ul class="tags">
+                <li>Arrival Transfer</li>
+                <li>Sightseeing tour</li>
+                <li>Lunch</li>
+                <li>Dinner</li>
+            </ul>
+        </div>
+        <div class="accordion-panel">
+            <h5>Other Benfits (On Arrival) </h5>
+            <ul class="Benfits">
+                <li><img style="width:35px" src="images/meal.svg" alt="meal">
+                    Breakfast
+                </li>
+                <li><img style="width:35px" src="images/transport.svg" alt="transport">
+                    Transfers
+                </li>
+                <li><img style="width:35px" src="images/hotel.svg" alt="star hotel">
+                    Stay Included
+                </li>
+            </ul>
+            <p>Drive to Nainital in the morning and on arrival check in at the hotel. evening free for leisure. Overnight stay in hotel.</p>
+        </div>
+    </div>
+    <div class="itinerary-cont-box accordion-item">
+        <span>Day 2</span>
+        <div class="accordion-thumb">
+            <h4>NAINITAL</h4>
+        </div>
+        <div class="accordion-panel" style="">
+            <p>After the breakfast, you will head for the famous lake tour of Bhimtal, Sattal, Naukuchiatal, Malli Tal. Overnight stay in Hotel.</p>
+        </div>
+    </div>
+    <div class="itinerary-cont-box accordion-item">
+        <span>Day 3</span>
+        <div class="accordion-thumb">
+            <h4>NAINITAL</h4>
+        </div>
+        <div class="accordion-panel">
+            <p>After having the breakfast, visit Snow View Point, Bara Pathar, Tiffin Top, Zoo. In the evening, enjoy shopping at the Mall Road. Overnight stay at the hotel.</p>
+        </div>
+    </div>
+    <div class="itinerary-cont-box accordion-item">
+        <span>Day 4</span>
+        <div class="accordion-thumb">
+            <h4>Nainital - Delhi</h4>
+        </div>
+        <div class="accordion-panel">
+            <p>After early breakfast, drive back to Delhi. On arrival to Delhi, drop at your place.</p>
+            <p><strong>Tour and Services ends</strong></p>
+        </div>
+    </div>
+</div>`
+},
+
+{
+    id: 2,
+    packageId: 10,
+    itinerary: `<div class="itinerary-cont accordion-box">
+    <h3>Itinerary</h3>
+    <div class="itinerary-cont-box accordion-item is-active">
+       <span>Day 1</span>
+             <div class="accordion-thumb">
+          <h4>DELHI - NAINITAL (320 KMS / 08 HRS)&lt;</h4>
+          <ul class="tags">
+             <li>Arrival Transfer</li>
+             <li>Sightseeing tour</li>                           
+             <li>Lunch</li>
+             <li>Dinner</li>
+          </ul>
+       </div>
+       <div class="accordion-panel">
+          <h5>Other Benfits (On Arrival) </h5>
+          <ul class="Benfits">
+             <li><img src="../../states/uttarakhand/image/meal.svg" alt="meal">
+                Breakfast
+             </li>
+             <li><img src="../../states/uttarakhand/image/transport.svg" alt="transport">
+                Transfers
+             </li>
+             <li><img src="../../states/uttarakhand/image/3-star.svg" alt="star hotel">
+                Stay Included
+             </li>
+          </ul>
+            <p>Morning pick from Delhi and drive to captivating Nainital. upon arrival Nainital check in at Hotel. evening free for leisure, Overnight stay at the hotel.</p>
+       </div>                    
+    </div>
+     <div class="itinerary-cont-box accordion-item">
+       <span>Day 2</span>
+             <div class="accordion-thumb">
+          <h4>Nainital</h4>
+       </div>
+       <div class="accordion-panel">
+          <p>After breakfast, start the lake tour of Nainital including Naukuchiatal, Bhimtal, Malli Tal and Khurpa Tal. Evening you can either do shopping in the Mall Road or enjoy boat ride in Naini Lake and visit Naina Devi Temple. Overnight stay at the hotel.</p>
+       </div>
+    </div>
+    <div class="itinerary-cont-box accordion-item">
+       <span>Day 3</span>
+             <div class="accordion-thumb">
+          <h4>Nainital - Delhi</h4>
+       </div>
+       <div class="accordion-panel">                        
+          <p>After breakfast head back to Delhi. Upon arrival at Delhi, drive to your home.</p>
+          <p><strong>Tour and Services ends</strong></p>
+       </div>
+    </div>        
+ </div>`
+}
+
+
+];
+
+
+
+// itineraries.map(function (iti) {
+//     //    creates a DOM element (card) and appends in the div with id - #app
+
+
+
+// });
+
