325bf1fc721738330541445ca2a1e1e3e6b0e50a
amit-tiwari-gcp <amit.tiwari.gcp@gmail.com>
2023-09-30 21:20:44 +0530

    sdfs

--- data.js
+++ data.js
@@ -350,7 +350,6 @@ const allPackages = [...packages,
     image: "https://images.unsplash.com/photo-1641584495061-89b9025f563b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
     description: "",
     suggested: [{}]
-}
 },
 {
     id: 21,
